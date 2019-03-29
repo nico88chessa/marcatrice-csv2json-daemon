6fc634ab8e64a6c1399cf28f77a077f3f4223e13
Nicola Chessa <n.chessa@dvoptic.com>
2019-03-29 09:26:04 +0100

    aggiunto file di risorse; Nota: il file .py e' generato dal rcc usando il comando pyside2-rcc

---
+++ ui/resource_rc.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+
+# Resource object code
+#
+# Created: gio mar 28 17:29:55 2019
+#      by: The Resource Compiler for PySide2 (Qt v5.12.2)
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide2 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x07}\
+Q\
+MainWindow {\x0d\x0a\x09b\
+ackground-color:\
+ white;\x0d\x0a    min\
+-width: 800px;\x0d\x0a\
+    min-height: \
+600px;\x0d\x0a}\x0d\x0a\x0d\x0a.MD\
+LineEdit {\x0d\x0a    \
+qproperty-labelF\
+ocusColor: rgba(\
+46, 125, 50, 87%\
+);\x0d\x0a    backgrou\
+nd-color: transp\
+arent;\x0d\x0a    bord\
+er-bottom-color:\
+ rgba(0, 0, 0, 4\
+2%);\x0d\x0a    color:\
+ rgba(0, 0, 0, 5\
+4%);\x0d\x0a}\x0d\x0a\x0d\x0a.MDLi\
+neEdit:hover {\x0d\x0a\
+    border-botto\
+m-color: rgba(0,\
+ 0, 120, 87%);\x0d\x0a\
+}\x0d\x0a\x0d\x0a.MDLineEdit\
+:focus:!read-onl\
+y {\x0d\x0a    color: \
+rgba(0, 0, 0, 87\
+%);\x0d\x0a    border-\
+bottom-color: rg\
+ba(46, 125, 50, \
+100%);\x0d\x0a}\x0d\x0a\x0d\x0a.MD\
+LineEdit:disable\
+d {\x0d\x0a    color: \
+rgba(0, 0, 0, 38\
+%);\x0d\x0a    border-\
+bottom-color: rg\
+ba(0, 0, 0, 42%)\
+;\x0d\x0a}\x0d\x0a\x0d\x0a.MDLineE\
+dit {\x0d\x0a    font-\
+size: 16px;\x0d\x0a   \
+ padding: 36px 0\
+px 8px;\x0d\x0a    max\
+-height: 54px;\x0d\x0a\
+    min-height: \
+18px;\x0d\x0a    qprop\
+erty-labelX: 8;\x0d\
+\x0a    qproperty-l\
+abelY: 28;\x0d\x0a    \
+qproperty-labelT\
+extWidth: 12;\x0d\x0a \
+   border: none;\
+\x0d\x0a    border-bot\
+tom-width: 1px;\x0d\
+\x0a    border-bott\
+om-style: solid;\
+\x0d\x0a}\x0d\x0a\x0d\x0a.MDLineEd\
+it:focus {\x0d\x0a    \
+padding: 36px 0p\
+x 7px;\x0d\x0a    bord\
+er-bottom-width:\
+ 2px;\x0d\x0a}\x0d\x0a\x0d\x0a.MDL\
+ineEdit:read-onl\
+y {\x0d\x0a    padding\
+: 36px 0px 8px;\x0d\
+\x0a    border-bott\
+om-width: 1px;\x0d\x0a\
+}\x0d\x0a\x0d\x0a.MDLineEdit\
+:disabled {\x0d\x0a   \
+ padding: 36px 0\
+px 7px;\x0d\x0a    bor\
+der-bottom-width\
+: 1px;\x0d\x0a    bord\
+er-bottom-style:\
+ dotted;\x0d\x0a}\x0d\x0a\x0d\x0a.\
+QPushButton {\x0d\x0a \
+   padding: 0px \
+16px;\x0d\x0a    borde\
+r-width: 1px;\x0d\x0a \
+   border-style:\
+ solid;\x0d\x0a    bor\
+der-radius: 2px;\
+\x0d\x0a    min-width:\
+ 64px;\x0d\x0a    min-\
+height: 32px;\x0d\x0a \
+   max-height: 3\
+2px;\x0d\x0a    font-s\
+ize: 14px;\x0d\x0a    \
+font-weight: 600\
+;\x0d\x0a}\x0d\x0a\x0d\x0a.QPushBu\
+tton {\x0d\x0a    qpro\
+perty-focusPolic\
+y: NoFocus;\x0d\x0a   \
+ border-color: t\
+ransparent;\x0d\x0a   \
+ color: white;\x0d\x0a\
+    background-c\
+olor: green;\x0d\x0a}\x0d\
+\x0a\x0d\x0a.QPushButton:\
+:on {\x0d\x0a    color\
+: red;\x0d\x0a}\x0d\x0a\x0d\x0a.QC\
+heckBox {\x0d\x0a    f\
+ont-size: 16px;\x0d\
+\x0a    padding: 8p\
+x 0px 8px;\x0d\x0a    \
+color: black;\x0d\x0a \
+   spacing: 32;\x0d\
+\x0a}\x0d\x0a\x0d\x0a.QCheckBox\
+:indicator {\x0d\x0a\x0d\x0a\
+}\x0d\x0a\x0d\x0a.QPlainText\
+Edit {\x0d\x0a    font\
+-size: 12px;\x0d\x0a  \
+  font-family: C\
+onsolas;\x0d\x0a    ba\
+ckground-color: \
+black;\x0d\x0a    colo\
+r: white;\x0d\x0a}\x0d\x0a\x0d\x0a\
+.QFrame {\x0d\x0a    m\
+argin: 8px;\x0d\x0a}\x0d\x0a\
+\x0d\x0a.QFrame > QWid\
+get {\x0d\x0a    margi\
+n: 8px;\x0d\x0a}\x0d\x0a\
+"
+
+qt_resource_name = b"\
+\x00\x03\
+\x00\x00x\xa3\
+\x00q\
+\x00s\x00s\
+\x00\x05\
+\x00z\xc0%\
+\x00s\
+\x00t\x00y\x00l\x00e\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
+\x00\x00\x00\x0c\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
